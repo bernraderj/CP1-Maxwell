27afe97730f1a6b8ee70454d1baa72a0ac4d0a48
david <david@david.at>
2021-01-16 12:35:43 +0100

    solving magnetization problems for 3 meshes

--- magnetized/magnetized.py
+++ magnetized/magnetized.py
@@ -3,12 +3,12 @@ from dolfin import *
 from ufl import nabla_div
 
 def main():
-    solver("horseshoe_magnet.xml", "horseshoe_magnet_physical_region.xml", "horseshoe_magnet_facet_region.xml", "magnetized_solution_horseshoe_magnet.pvd")
-    solver("sphere_magnet.xml", "sphere_magnet_physical_region.xml", "sphere_magnet_facet_region.xml", "sphere_solution_horseshoe_magnet.pvd")
-    solver("cube_magnet.xml", "cube_magnet_physical_region.xml", "cube_magnet_facet_region.xml", "magnetized_solution_cube_magnet.pvd")
+    solver("horseshoe_magnet.xml", "horseshoe_magnet_physical_region.xml", "horseshoe_magnet_facet_region.xml", "magnetized_solution_horseshoe_magnet.pvd", "potential_horseshoe_magnet.pvd")
+    solver("sphere_magnet.xml", "sphere_magnet_physical_region.xml", "sphere_magnet_facet_region.xml", "magnetized_solution_sphere_magnet.pvd", "potential_sphere_magnet.pvd")
+    solver("cube_magnet.xml", "cube_magnet_physical_region.xml", "cube_magnet_facet_region.xml", "magnetized_solution_cube_magnet.pvd", "potential_cube_magnet.pvd")
     
 
-def solver(mesh_file, cell_regions_file, facet_regions_file, output_file):
+def solver(mesh_file, cell_regions_file, facet_regions_file, output_file, output_file_potential):
 
     # Import mesh with volume and facet regions
     mesh = Mesh(mesh_file)
@@ -24,60 +24,25 @@ def solver(mesh_file, cell_regions_file, facet_regions_file, output_file):
     #Magnetization
     M0 = Expression(('1.0', '0.0', '0.0'), degree=0)
     M1 = Expression(('0.0', '0.0', '0.0'), degree=0)
-    #test = dot(M0, M1)
 
     # Define variational problem
     u = TrialFunction(V) # scalar potential
     v = TestFunction(V)
 
-    #f0 = div(M0)
-    #f1 = div(M1)
-    #f0 = M0(0)
-    #f0 = Expression('div(M0)', degree=0)
-    #f0 = div(M0)
-    #f1 = M1(0)
-    #TODO: Bugfixing
-    #f0 = project(div(M0), V)
-    #f1 = project(div(M1), V)
     f0 = M0
     f1 = M1
 
-
     u = TrialFunction(V)
     v = TestFunction(V)
-    #f = Constant(0)
     a = (dot(grad(u), grad(v)))*dx
-    #L = f*v*dx(0) + f*v*dx(1)
-    #L = 1*v*dx(0) + 1*v*dx(1)
-    #L = (dot(f0, grad(v)))*dx(0) + (dot(f1, grad(v)))*dx(1)
-    #L = (dot(f0, f1))*dx(0) + (dot(f1, f1))*dx(1)
-    #L = (dot(grad(v), grad(v)))*dx(0) + (dot(grad(v), grad(v)))*dx(1)
-    #L = (dot(grad(v), grad(v)))*dx
-    L = (dot(f0, grad(v)))*dx(0) + (dot(f1, grad(v)))*dx(1)
+    L = (dot(f0, grad(v)))*dx(1) + (dot(f1, grad(v)))*dx(2)
 
-    # Dirichlet boundary (1 at (0,0,0))
-    bc1 = DirichletBC(V, Constant(1.0), facet_regions, 3)
-
-    #TODO: smaller tol without No-Facets-Warning
-    '''
-    def origin(x, on_boundary):
-        #tol = 1E-14
-        tol = 5
-        return (near(x[0],0,tol) and near(x[1],0,tol) and near(x[2],0,tol))
-        #tol = 2E-1
-        #return near(x[0], 0, tol) #and near(x[1], 0, tol) and near(x[2], 0, tol)
-        #return (near(x[0],0,tol) and near(x[1],0,tol) and near(x[2],0,tol))
-
-        #return near(x[0],0,DOLFIN_EPS)
-        #return near(x[0],0,DOLFIN_EPS) and near(x[1],0,DOLFIN_EPS) and near(x[2],0,DOLFIN_EPS)
-
-    #bc2 = DirichletBC(V, Constant(1.0), origin)
-    '''
+    # Dirichlet boundary (0 at outer boundary)
+    bc1 = DirichletBC(V, Constant(0.0), facet_regions, 3)
 
     # Compute solution
     u = Function(V)
     solve(a == L, u, bc1)
-
 
     #stray field
     H = project(-grad(u), W)
@@ -85,5 +50,8 @@ def solver(mesh_file, cell_regions_file, facet_regions_file, output_file):
     #Export
     vtkfile = File(output_file)
     vtkfile << H
+
+    potentialfile = File(output_file_potential)
+    potentialfile << u
 
 main()
