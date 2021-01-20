44f4d253c26d7fd6842f90acbad67508e3b557d3
david <david@david.at>
2021-01-20 12:55:30 +0100

    different airbox sizes

--- magnetized/magnetized.py
+++ magnetized/magnetized.py
@@ -3,9 +3,12 @@ from dolfin import *
 from ufl import nabla_div
 
 def main():
-    solver("horseshoe_magnet.xml", "horseshoe_magnet_physical_region.xml", "horseshoe_magnet_facet_region.xml", "magnetized_solution_horseshoe_magnet.pvd", "potential_horseshoe_magnet.pvd")
-    solver("sphere_magnet.xml", "sphere_magnet_physical_region.xml", "sphere_magnet_facet_region.xml", "magnetized_solution_sphere_magnet.pvd", "potential_sphere_magnet.pvd")
-    solver("cube_magnet.xml", "cube_magnet_physical_region.xml", "cube_magnet_facet_region.xml", "magnetized_solution_cube_magnet.pvd", "potential_cube_magnet.pvd")
+    #solver("horseshoe_magnet.xml", "horseshoe_magnet_physical_region.xml", "horseshoe_magnet_facet_region.xml", "magnetized_solution_horseshoe_magnet.pvd", "potential_horseshoe_magnet.pvd")
+    #solver("sphere_magnet.xml", "sphere_magnet_physical_region.xml", "sphere_magnet_facet_region.xml", "magnetized_solution_sphere_magnet.pvd", "potential_sphere_magnet.pvd")
+    #solver("cube_magnet.xml", "cube_magnet_physical_region.xml", "cube_magnet_facet_region.xml", "magnetized_solution_cube_magnet.pvd", "potential_cube_magnet.pvd")
+    solver("cube_magnet_30.xml", "cube_magnet_30_physical_region.xml", "cube_magnet_30_facet_region.xml", "30_magnetized_solution_cube_magnet.pvd", "30_potential_cube_magnet.pvd")
+    solver("cube_magnet_50.xml", "cube_magnet_50_physical_region.xml", "cube_magnet_50_facet_region.xml", "50_magnetized_solution_cube_magnet.pvd", "50_potential_cube_magnet.pvd")
+    #solver("cube_magnet_70.xml", "cube_magnet_70_physical_region.xml", "cube_magnet_70_facet_region.xml", "70_magnetized_solution_cube_magnet.pvd", "70_potential_cube_magnet.pvd")
     
 
 def solver(mesh_file, cell_regions_file, facet_regions_file, output_file, output_file_potential):
@@ -43,9 +46,11 @@ def solver(mesh_file, cell_regions_file, facet_regions_file, output_file, output
     # Compute solution
     u = Function(V)
     solve(a == L, u, bc1)
+    #solve(a == L, u, bc1, solver_parameters={'linear_solver':'mumps'})
 
     #stray field
-    H = project(-grad(u), W)
+    #H = project(-grad(u), W)
+    H = project(-grad(u), W, solver_type="mumps")
 
     #Export
     vtkfile = File(output_file)
