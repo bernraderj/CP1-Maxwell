5f1ed07084d7391b1a430936416daf2e0d37a2d0
david <david@david.at>
2021-01-24 16:10:27 +0100

    abgabe: adapting solver calls to abgabe mesh files

--- abgabe/magnetized/convert_meshes_to_pvd.py
+++ abgabe/magnetized/convert_meshes_to_pvd.py
@@ -10,10 +10,12 @@ def convert(mesh_file, cell_regions_file, facet_regions_file, output_file):
 
 
 #convert("horseshoe_magnet.xml", "horseshoe_magnet_physical_region.xml", "horseshoe_magnet_facet_region.xml", "domains_horseshoe.pvd")
-#convert("sphere_magnet.xml", "sphere_magnet_physical_region.xml", "sphere_magnet_facet_region.xml", "domains_sphere.pvd")
+convert("sphere_magnet.xml", "sphere_magnet_physical_region.xml", "sphere_magnet_facet_region.xml", "domains_sphere.pvd")
 #convert("cube_magnet.xml", "cube_magnet_physical_region.xml", "cube_magnet_facet_region.xml", "domains_cube.pvd")
 
-convert("cube_magnet_30.xml", "cube_magnet_30_physical_region.xml", "cube_magnet_30_facet_region.xml", "30_domains_cube.pvd")
-convert("cube_magnet_50.xml", "cube_magnet_50_physical_region.xml", "cube_magnet_50_facet_region.xml", "50_domains_cube.pvd")
-convert("cube_magnet_70.xml", "cube_magnet_70_physical_region.xml", "cube_magnet_70_facet_region.xml", "70_domains_cube.pvd")
+#convert("cube_magnet_30.xml", "cube_magnet_30_physical_region.xml", "cube_magnet_30_facet_region.xml", "30_domains_cube.pvd")
+#convert("cube_magnet_50.xml", "cube_magnet_50_physical_region.xml", "cube_magnet_50_facet_region.xml", "50_domains_cube.pvd")
+#convert("cube_magnet_70.xml", "cube_magnet_70_physical_region.xml", "cube_magnet_70_facet_region.xml", "70_domains_cube.pvd")
+
+
 
--- abgabe/magnetized/magnetized.py
+++ abgabe/magnetized/magnetized.py
@@ -4,10 +4,10 @@ from ufl import nabla_div
 
 def main():
     #solver("horseshoe_magnet.xml", "horseshoe_magnet_physical_region.xml", "horseshoe_magnet_facet_region.xml", "magnetized_solution_horseshoe_magnet.pvd", "potential_horseshoe_magnet.pvd")
-    #solver("sphere_magnet.xml", "sphere_magnet_physical_region.xml", "sphere_magnet_facet_region.xml", "magnetized_solution_sphere_magnet.pvd", "potential_sphere_magnet.pvd")
+    solver("sphere_magnet.xml", "sphere_magnet_physical_region.xml", "sphere_magnet_facet_region.xml", "magnetized_solution_sphere_magnet.pvd", "potential_sphere_magnet.pvd")
     #solver("cube_magnet.xml", "cube_magnet_physical_region.xml", "cube_magnet_facet_region.xml", "magnetized_solution_cube_magnet.pvd", "potential_cube_magnet.pvd")
-    solver("cube_magnet_30.xml", "cube_magnet_30_physical_region.xml", "cube_magnet_30_facet_region.xml", "30_magnetized_solution_cube_magnet.pvd", "30_potential_cube_magnet.pvd")
-    solver("cube_magnet_50.xml", "cube_magnet_50_physical_region.xml", "cube_magnet_50_facet_region.xml", "50_magnetized_solution_cube_magnet.pvd", "50_potential_cube_magnet.pvd")
+    #solver("cube_magnet_30.xml", "cube_magnet_30_physical_region.xml", "cube_magnet_30_facet_region.xml", "30_magnetized_solution_cube_magnet.pvd", "30_potential_cube_magnet.pvd")
+    #solver("cube_magnet_50.xml", "cube_magnet_50_physical_region.xml", "cube_magnet_50_facet_region.xml", "50_magnetized_solution_cube_magnet.pvd", "50_potential_cube_magnet.pvd")
     #solver("cube_magnet_70.xml", "cube_magnet_70_physical_region.xml", "cube_magnet_70_facet_region.xml", "70_magnetized_solution_cube_magnet.pvd", "70_potential_cube_magnet.pvd")
     
 
@@ -35,8 +35,6 @@ def solver(mesh_file, cell_regions_file, facet_regions_file, output_file, output
     f0 = M0
     f1 = M1
 
-    u = TrialFunction(V)
-    v = TestFunction(V)
     a = (dot(grad(u), grad(v)))*dx
     L = (dot(f0, grad(v)))*dx(1) + (dot(f1, grad(v)))*dx(2)
 
